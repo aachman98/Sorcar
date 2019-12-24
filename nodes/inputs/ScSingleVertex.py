a229c55fc3a4a55c9d24821b28321beb88046de3
Punya Aachman <pa@localhost.localdomain>
2019-12-24 23:29:32 +0530

    Updated "Scene Info" & "Single Vertex" nodes

--- nodes/inputs/ScSingleVertex.py
+++ nodes/inputs/ScSingleVertex.py
@@ -1,6 +1,8 @@
 import bpy
 
+from bpy.props import BoolProperty
 from bpy.types import Node
+from .._base.node_base import ScNode
 from .._base.node_input import ScInputNode
 
 
@@ -8,15 +10,19 @@ class ScSingleVertex(Node, ScInputNode):
     bl_idname = "ScSingleVertex"
     bl_label = "Single Vertex"
 
+    in_show_name: BoolProperty(update=ScNode.update_value)
+
+    def init(self, context):
+        super().init(context)
+        self.inputs.new("ScNodeSocketBool", "Show Name").init("in_show_name", True)
+
     def functionality(self):
-        name = self.inputs["Name"].default_value
-        me = bpy.data.meshes.new(name + "_mesh")
-        obj = bpy.data.objects.new(name, me)
+        m = bpy.data.meshes.new(self.inputs["Name"].default_value)
+        obj = bpy.data.objects.new(self.inputs["Name"].default_value, m)
         obj.location = bpy.context.scene.cursor.location
-        me.from_pydata([(0,0,0)], [], [])
-        # obj.show_name = True
-        me.update()
-        # Link object to the active collection
+        m.vertices.add(1)
+        m.update()
+        obj.show_name = self.inputs["Show Name"].default_value
         bpy.context.collection.objects.link(obj)
         bpy.context.view_layer.objects.active = obj
 
