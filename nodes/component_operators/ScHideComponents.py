0ac17e100eabfad26b3649c60ea6b38d98833eea
CORPSE-SYS <58483370+CORPSE-SYS@users.noreply.github.com>
2019-12-27 11:23:25 +0100

    Hide/Unhide Components

---
+++ nodes/component_operators/ScHideComponents.py
@@ -0,0 +1,21 @@
+import bpy
+
+from bpy.props import BoolProperty
+from bpy.types import Node
+from .._base.node_base import ScNode
+from .._base.node_operator import ScEditOperatorNode
+
+class ScHideComponents(Node, ScEditOperatorNode):
+    bl_idname = "ScHideComponents"
+    bl_label = "Hide Components"
+    
+    in_unselected: BoolProperty(default=False, update=ScNode.update_value)
+
+    def init(self, context):
+        super().init(context)
+        self.inputs.new("ScNodeSocketBool", "Unselected").init("in_unselected")
+
+    def functionality(self):
+        bpy.ops.mesh.hide(
+            unselected = self.inputs["Unselected"].default_value,
+            )
---
+++ nodes/component_operators/ScUnhideComponents.py
@@ -0,0 +1,21 @@
+import bpy
+
+from bpy.props import BoolProperty
+from bpy.types import Node
+from .._base.node_base import ScNode
+from .._base.node_operator import ScEditOperatorNode
+
+class ScUnhideComponents(Node, ScEditOperatorNode):
+    bl_idname = "ScUnhideComponents"
+    bl_label = "Unhide Components"
+    
+    in_select: BoolProperty(default=True, update=ScNode.update_value)
+
+    def init(self, context):
+        super().init(context)
+        self.inputs.new("ScNodeSocketBool", "Select").init("in_select")
+    
+    def functionality(self):
+        bpy.ops.mesh.reveal(
+            select = self.inputs["Select"].default_value,
+            )
