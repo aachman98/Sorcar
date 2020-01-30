7f30a88ef82d396c415db6bf753793795706c500
Tomoaki Nakano <kanotomo@users.noreply.github.com>
2020-01-30 13:24:59 +0900

    Improve Parent node and add Clear Parent, Get Parent, Get Children node.

---
+++ nodes/object_operators/ScGetParent.py
@@ -0,0 +1,25 @@
+import bpy
+
+from bpy.types import Node
+from .._base.node_base import ScNode
+from ...helper import focus_on_object
+
+class ScGetParent(Node, ScNode):
+    bl_idname = "ScGetParent"
+    bl_label = "Get Parent"
+    
+    def init(self, context):
+        super().init(context)
+        self.inputs.new("ScNodeSocketObject", "Object")
+        self.outputs.new("ScNodeSocketObject", "Parent")
+    
+    def error_condition(self):
+        return (
+            self.inputs["Object"].default_value == None
+        )
+    
+    def pre_execute(self):
+        focus_on_object(self.inputs["Object"].default_value)
+    
+    def post_execute(self):
+        return {"Parent": self.inputs["Object"].default_value.parent}
