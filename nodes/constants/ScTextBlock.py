774896d6d8b5140f642a2c274ebd9a06df8de6d2
Punya Aachman <aachman98@gmail.com>
2020-05-24 09:22:42 +0530

    Added "Text Block" constant input node

---
+++ nodes/constants/ScTextBlock.py
@@ -0,0 +1,29 @@
+import bpy
+
+from bpy.props import PointerProperty
+from bpy.types import Node
+from .._base.node_base import ScNode
+
+class ScTextBlock(Node, ScNode):
+    bl_idname = "ScTextBlock"
+    bl_label = "Text Block"
+
+    prop_text: PointerProperty(name="Text", type=bpy.types.Text, update=ScNode.update_value)
+
+    def init(self, context):
+        super().init(context)
+        self.outputs.new("ScNodeSocketString", "Value")
+    
+    def error_condition(self):
+        return self.prop_text == None
+    
+    def draw_buttons(self, context, layout):
+        super().draw_buttons(context, layout)
+        layout.prop(self, "prop_text")
+        if (not self.prop_text == None):
+            col = layout.column()
+            for l in self.prop_text.lines:
+                col.label(text=l.body)
+    
+    def post_execute(self):
+        return {"Value": self.prop_text.as_string()}
