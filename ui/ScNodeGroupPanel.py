c196c575f10e6cfc95911f19742b42d41a3ee6e6
Punya Aachman <aachman98@gmail.com>
2020-05-31 02:51:00 +0530

    Added create/edit group ops to "Node Groups" panel

--- ui/ScNodeGroupPanel.py
+++ ui/ScNodeGroupPanel.py
@@ -4,10 +4,10 @@ from bpy.types import Panel
 from ._base.panel_base import ScPanel
 
 class ScNodeGroupPanel(Panel, ScPanel):
-    """Create/Edit Sorcar Node-Groups"""
     bl_label = "Node Groups"
     bl_idname = "NODE_PT_sc_node_group"
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="Hello world!", icon='WORLD_DATA')
+        layout.operator("sorcar.group_nodes", text="Create Group", icon='GROUP')
+        layout.operator("sorcar.edit_group", text="Edit Group", icon='MENU_PANEL')
