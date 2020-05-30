a2eb52c1eb1a4bbb294e48d24db4b9d58e56b0e1
Punya Aachman <aachman98@gmail.com>
2020-05-31 03:15:22 +0530

    Improved "Properties" panel: preview node label

--- ui/ScTreePropertiesPanel.py
+++ ui/ScTreePropertiesPanel.py
@@ -6,7 +6,10 @@ from ._base.panel_base import ScPanel
 class ScTreePropertiesPanel(Panel, ScPanel):
     bl_label = "Properties"
     bl_idname = "NODE_PT_sc_tree_properties"
+    bl_order = 0
 
     def draw(self, context):
         layout = self.layout
-        layout.prop(context.space_data.node_tree, "prop_realtime")
+        nt = context.space_data.node_tree
+        layout.label(text="Preview node: " + str(nt.node), icon='NODE')
+        layout.prop(nt, "prop_realtime")
