23812758cd582e374e077215b5cf4d7ae922f8a9
Punya Aachman <aachman98@gmail.com>
2020-06-07 04:55:17 +0530

    Improve "Duplicate Object" node: added linked prop

--- nodes/object_operators/ScDuplicateObject.py
+++ nodes/object_operators/ScDuplicateObject.py
@@ -1,6 +1,6 @@
 import bpy
 
-from bpy.props import FloatProperty
+from bpy.props import BoolProperty
 from bpy.types import Node
 from .._base.node_base import ScNode
 from .._base.node_operator import ScObjectOperatorNode
@@ -9,9 +9,12 @@ from ...helper import remove_object
 class ScDuplicateObject(Node, ScObjectOperatorNode):
     bl_idname = "ScDuplicateObject"
     bl_label = "Duplicate Object"
+
+    in_linked: BoolProperty(update=ScNode.update_value)
     
     def init(self, context):
         super().init(context)
+        self.inputs.new("ScNodeSocketBool", "Linked").init("in_linked")
         self.outputs.new("ScNodeSocketObject", "Duplicate Object")
     
     def pre_execute(self):
@@ -19,7 +22,9 @@ class ScDuplicateObject(Node, ScObjectOperatorNode):
         super().pre_execute()
     
     def functionality(self):
-        bpy.ops.object.duplicate()
+        bpy.ops.object.duplicate(
+            linked = self.inputs["Linked"].default_value
+        )
     
     def post_execute(self):
         out = super().post_execute()
