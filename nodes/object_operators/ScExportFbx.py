28ba915fcb381689cdcbfc9872b39995cdefebea
Punya Aachman <aachman98@gmail.com>
2020-06-07 06:36:08 +0530

    Improved "Export FBX" object operator node

--- nodes/object_operators/ScExportFbx.py
+++ nodes/object_operators/ScExportFbx.py
@@ -10,21 +10,27 @@ class ScExportFbx(Node, ScObjectOperatorNode):
     bl_idname = "ScExportFbx"
     bl_label = "Export FBX"
 
-    in_file: StringProperty(subtype='FILE_PATH', update=ScNode.update_value)
+    in_file: StringProperty(subtype='DIR_PATH', update=ScNode.update_value)
+    in_filename: StringProperty(default="untitled")
 
     def init(self, context):
         super().init(context)
         self.inputs.new("ScNodeSocketString", "File").init("in_file", True)
+        self.inputs.new("ScNodeSocketString", "File Name").init("in_filename", True)
     
     def error_condition(self):
         return (
             super().error_condition()
             or self.inputs["File"].default_value == ""
+            or self.inputs["File Name"].default_value == ""
         )
     
     def functionality(self):
         bpy.ops.export_scene.fbx(
-            filepath = bpy.path.abspath(self.inputs["File"].default_value),
+            filepath = os.path.join(
+                bpy.path.abspath(self.inputs["File"].default_value),
+                self.inputs["File Name"].default_value+".fbx"
+            ),
             use_selection = True,
             use_tspace = True
         )
