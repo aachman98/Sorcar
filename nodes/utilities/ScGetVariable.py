d1d287ad5b097cd881cd6b2456af111de1498963
Punya Aachman <aachman98@gmail.com>
2020-06-08 21:47:27 +0530

    Improved "Get Variable" node: default value

--- nodes/utilities/ScGetVariable.py
+++ nodes/utilities/ScGetVariable.py
@@ -10,10 +10,12 @@ class ScGetVariable(Node, ScNode):
 
     prop_nodetree: PointerProperty(name="NodeTree", type=bpy.types.NodeTree, update=ScNode.update_value)
     in_name: StringProperty(default="Var", update=ScNode.update_value)
+    in_default: StringProperty(default="Value", update=ScNode.update_value)
 
     def init(self, context):
         super().init(context)
         self.inputs.new("ScNodeSocketString", "Name").init("in_name", True)
+        self.inputs.new("ScNodeSocketString", "Default").init("in_default")
         self.outputs.new("ScNodeSocketUniversal", "Value")
     
     def draw_buttons(self, context, layout):
@@ -29,7 +31,8 @@ class ScGetVariable(Node, ScNode):
     def post_execute(self):
         variables = eval(self.prop_nodetree.variables)
         if (not self.inputs["Name"].default_value in variables):
-            return None
+            variables[self.inputs["Name"].default_value] = self.inputs["Default"].default_value
+            self.prop_nodetree.variables = repr(variables)
         out = {}
         out["Value"] = variables[self.inputs["Name"].default_value]
         return out
