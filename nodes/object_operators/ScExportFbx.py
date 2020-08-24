ffa22f31a759de6a4ec33861d0a8ca8706a8d5e5
Punya Aachman <aachman98@gmail.com>
2020-08-24 20:02:15 +0530

    Updated "Export FBX": Renamed input to "Directory"

--- nodes/object_operators/ScExportFbx.py
+++ nodes/object_operators/ScExportFbx.py
@@ -10,25 +10,25 @@ class ScExportFbx(Node, ScObjectOperatorNode):
     bl_idname = "ScExportFbx"
     bl_label = "Export FBX"
 
-    in_file: StringProperty(subtype='DIR_PATH', update=ScNode.update_value)
+    in_dir: StringProperty(subtype='DIR_PATH', update=ScNode.update_value)
     in_filename: StringProperty(default="untitled")
 
     def init(self, context):
         super().init(context)
-        self.inputs.new("ScNodeSocketString", "File").init("in_file", True)
+        self.inputs.new("ScNodeSocketString", "Directory").init("in_dir", True)
         self.inputs.new("ScNodeSocketString", "File Name").init("in_filename", True)
     
     def error_condition(self):
         return (
             super().error_condition()
-            or self.inputs["File"].default_value == ""
+            or self.inputs["Directory"].default_value == ""
             or self.inputs["File Name"].default_value == ""
         )
     
     def functionality(self):
         bpy.ops.export_scene.fbx(
             filepath = os.path.join(
-                bpy.path.abspath(self.inputs["File"].default_value),
+                bpy.path.abspath(self.inputs["Directory"].default_value),
                 self.inputs["File Name"].default_value+".fbx"
             ),
             use_selection = True,
