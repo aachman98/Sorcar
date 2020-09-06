4e38d06a869423e5039e26d98669d03a6a5f1c48
Punya Aachman <aachman98@gmail.com>
2020-09-06 15:01:18 +0530

    Improved debug: get/set timestamp in mem with logs

--- debug.py
+++ debug.py
@@ -4,6 +4,7 @@ import traceback
 from datetime import datetime
 
 sc_logs = [] # Sample log: ("SORCAR [2020-08-24 18:51:23,143602] (INFO): ...log message...", 1)
+mem_time = []
 
 def str_to_level(str='INFO'):
     if str == 'NONE':
@@ -25,8 +26,20 @@ def level_to_str(level=1):
     elif level == 3:
         return 'TRACE'
 
-def log(parent=None, child=None, func=None, msg="", level=1):
-    log = "SORCAR " + datetime.now().strftime('[%d-%h-%Y %T,%f]') + " (" + level_to_str(level) + "): "
+def log(parent=None, child=None, func=None, msg="", level=1, mem='NONE'):
+    # Set current time & delta time (in seconds)
+    time_now = datetime.now()
+    time_msg = None
+    if (mem == 'SET'):
+        mem_time.append(time_now)
+        time_msg = "Recording time..."
+    elif (mem == 'GET'):
+        if (len(mem_time) > 0):
+            time_msg = "Time taken = "+str((time_now - mem_time.pop()).total_seconds())+" seconds"
+        else:
+            time_msg = "'mem_time' empty"
+    # Create log (string) and append to list along with its level
+    log = "SORCAR " + time_now.strftime('[%d-%h-%Y %T,%f]') + " (" + level_to_str(level) + "): "
     if (parent):
         log += parent + ": "
     if (child):
@@ -34,7 +47,10 @@ def log(parent=None, child=None, func=None, msg="", level=1):
     if (func):
         log += func + "(): "
     log += msg
+    if (time_msg):
+        log += " (" + time_msg + ")"
     sc_logs.append((log, level))
+    # Try to get minimum log level from user prefs (default to 1)
     try:
         min_level = str_to_level(bpy.context.preferences.addons.get(__package__).preferences.log_level)
     except:
