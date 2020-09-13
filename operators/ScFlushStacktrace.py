52543e82218b1f71c658b8a990a68d5468899c36
Punya Aachman <aachman98@gmail.com>
2020-09-13 19:45:05 +0530

    Improved "Flush Stacktrace" op: enum log property

--- operators/ScFlushStacktrace.py
+++ operators/ScFlushStacktrace.py
@@ -2,17 +2,35 @@ import bpy
 import time
 
 from bpy.types import Operator
-from bpy.props import IntProperty, BoolProperty
+from bpy.props import EnumProperty
 from ..helper import sc_poll_op
-from ..debug import flush_logs
+from ..debug import flush_logs, str_to_level
 
 class ScFlushStacktrace(Operator):
     """Output the full stacktrace for the last execution of node-tree"""
     bl_idname = "sorcar.flush_stacktrace"
     bl_label = "Flush Stacktrace"
 
-    prop_level: IntProperty(name="Log Level", default=2, min=1, max=3)
-    prop_save: BoolProperty(name="Save as Text Block", default=False)
+    prop_level: EnumProperty(
+    name = "Log Level",
+    description = "Maximum level of logs to output",
+    items = [
+        ('INFO', '1: Info', 'Register/unregister, addon-updater, versions, etc.'),
+        ('DEBUG', '2: Dataflow', 'Node-trees, node-groups, nodes, sockets, etc.'),
+        ('TRACE', '3: All', 'Node functions, socket functions, error conditions, etc.')
+    ],
+    default = 'DEBUG',
+    )
+
+    prop_save: EnumProperty(
+    name = "Output",
+    items = [
+        ('CONSOLE', 'Console', 'Print logs to external console/terminal only'),
+        ('TEXT', 'Text-Block', 'Save as internal text block only'),
+        ('BOTH', 'Both', 'Print to console and save as text')
+    ],
+    default = 'CONSOLE',
+    )
 
     @classmethod
     def poll(cls, context):
@@ -23,18 +41,21 @@ class ScFlushStacktrace(Operator):
 
     def execute(self, context):
         curr_tree = context.space_data.edit_tree
-        logs = flush_logs(self.prop_level)
+        logs = flush_logs(str_to_level(self.prop_level))
         if (len(logs) > 0):
             str = "sc_" + time.strftime('%Y-%m-%d_%H-%M-%S') + ".log"
-            if (self.prop_save):
+            if (self.prop_save in ['TEXT', 'BOTH']):
                 text = bpy.data.texts.new(str)
-            print("#"*48)
-            print("#"*10+" "+str+" "+"#"*10)
-            print("#"*48)
+            if (self.prop_save in ['CONSOLE', 'BOTH']):
+                print("#"*48)
+                print("#"*10+" "+str+" "+"#"*10)
+                print("#"*48)
             for log in logs:
-                print(log)
-                if (self.prop_save):
+                if (self.prop_save in ['CONSOLE', 'BOTH']):
+                    print(log)
+                if (self.prop_save in ['TEXT', 'BOTH']):
                     text.write(log + '\n')
-            print("#"*48)
+            if (self.prop_save in ['CONSOLE', 'BOTH']):
+                print("#"*48)
             return {'FINISHED'}
         return {'CANCELLED'}
