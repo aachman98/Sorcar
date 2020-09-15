2190ca53721bcd94811561d37709e5d37c777cf7
Punya Aachman <aachman98@gmail.com>
2020-09-16 00:57:12 +0530

    Added support for category icon, menu order

--- tree/ScNodeCategory.py
+++ tree/ScNodeCategory.py
@@ -4,5 +4,35 @@ from nodeitems_utils import NodeCategory
 
 class ScNodeCategory(NodeCategory):
     @classmethod
-    def poll(self, context):
+    def poll(cls, context):
         return context.space_data.tree_type == "ScNodeTree"
+    
+    def __init__(self, identifier, name, description="", items=None, icon_value=0):
+        self.icon_value = icon_value
+        super().__init__(identifier, name, description, items)
+    
+    def draw(self, context, layout):
+        layout.menu("NODE_MT_category_%s" % self.identifier, icon_value=self.icon_value)
+    
+    def add_node_menu():
+        return [
+            "inputs",
+            "curves",
+            None,
+            "transform",
+            "selection",
+            "deletion",
+            None,
+            "component_operators",
+            "object_operators",
+            "modifiers",
+            None,
+            "constants",
+            "arrays",
+            "noise",
+            None,
+            "utilities",
+            "settings",
+            None,
+            "flow_control"
+        ]
